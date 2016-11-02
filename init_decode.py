6a043e91455d591a236d612da6742a757d744428
Chong Shao <tingleshao@gmail.com>
2016-11-02 12:08:03 -0400

    update to support n decodings by param

--- init_decode.py
+++ init_decode.py
@@ -2,6 +2,7 @@
 import os
 import subprocess
 import time
+import sys
 
 
 class init_decode:
@@ -16,19 +17,17 @@ class init_decode:
         cmd = '/home/cshao/Downloads/Video_Codec_SDK_7.0.1/Samples/NvDecodeGL/NvDecodeGL -i=/home/cshao/Downloads/Video_Codec_SDK_7.0.1/Samples/common/video/plush1_720p_10s.m2v'
         os.system(cmd)
 
-    def call_nv_decoder_n(self):
+    def call_nv_decoder_n(self, n):
         processes = set()
         cmd = ['/home/cshao/Downloads/Video_Codec_SDK_7.0.1/Samples/NvDecodeGL/NvDecodeGL -i=/home/cshao/Downloads/Video_Codec_SDK_7.0.1/Samples/common/video/plush1_720p_10s.m2v']
-        processes.add(subprocess.Popen(cmd, shell=True))
-        processes.add(subprocess.Popen(cmd, shell=True))
-        processes.add(subprocess.Popen(cmd, shell=True))
-        processes.add(subprocess.Popen(cmd, shell=True))
+        for i in xrange(n):
+            processes.add(subprocess.Popen(cmd, shell=True))
 
 
 def main():
     idec = init_decode()
-    idec.call_nv_decoder_n()
-
+    n = int(sys.argv[1])
+    idec.call_nv_decoder_n(n)
 
 if __name__ == "__main__":
     main()
